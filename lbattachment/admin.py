450742b4fc4599a8f6c8c62f87652757f132797b
vicalloy <zbirder@gmail.com>
2016-08-06 16:44:29 +0800

    fix admin.py (user to created_by)

--- lbattachment/admin.py
+++ lbattachment/admin.py
@@ -7,9 +7,9 @@ from .models import LBAttachment
 
 
 class LBAttachmentAdmin(admin.ModelAdmin):
-    search_fields = ('user__username', 'filename', 'act_members_param', 'notice_members_param', 'share_members_param')
-    list_display = ('user', 'filename', 'is_img', 'num_downloads', 'is_active', )
+    search_fields = ('created_by__username', 'filename', 'act_members_param', 'notice_members_param', 'share_members_param')
+    list_display = ('created_by', 'filename', 'is_img', 'num_downloads', 'is_active', )
     list_filter = ('is_img', 'suffix')
-    raw_id_fields = ('user',)
+    raw_id_fields = ('created_by',)
 
 admin.site.register(LBAttachment, LBAttachmentAdmin)
